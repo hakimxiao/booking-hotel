ef2d71913d8abaaa22624bc3cacb0b57b8730fbe
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-07-03 13:22:33 +0700

    inisialisasi dan instalasi project

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Raleway } from "next/font/google";
+import "./globals.css";
+
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Online Booking Hotel",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${raleway.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
