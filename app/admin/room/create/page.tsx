1f1e861ebe5502c953a32ec352c881a0e1611866
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-07-10 16:51:22 +0700

    membuat komponen createForm untuk melakukkan create room pada role admin

--- app/admin/room/create/page.tsx
+++ app/admin/room/create/page.tsx
@@ -1,6 +1,10 @@
+import CreateForm from "@/components/admin/room/CreateForm"
+
 const CreateRoomPage = () => {
     return (
-        <div>CreateRoomPage</div>
+        <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
+            <CreateForm />
+        </div>
     )
 }
 
---
+++ components/admin/room/CreateForm.tsx
@@ -0,0 +1,61 @@
+import { IoCloudUploadOutline } from 'react-icons/io5';
+
+const CreateForm = () => {
+    return (
+        <form action="">
+            <div className="grid md:grid-cols-12 gap-5">
+                <div className="col-span-8 bg-white p-4">
+                    <div className="mb-4">
+                        <input type="text" name="name" className="py-2 px-4 rounded-sm border border-gray-400 w-full" placeholder="Room Name" />
+                        <div aria-live="polite" aria-atomic="true">
+                            <span className="text-sm text-red-500 mt-2">message</span>
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <textarea name="description" rows={8} className="py-2 px-4 rounded-sm border border-gray-400 w-full" placeholder="Description"></textarea>
+                        <div aria-live="polite" aria-atomic="true">
+                            <span className="text-sm text-red-500 mt-2">message</span>
+                        </div>
+                    </div>
+                    <div className="mb-4 grid md:grid-cols-3">
+                        <input type="checkbox" name="amenities" className="w-4 h-4 text-blue-600 bg-gray-100 border border-gray-300 rounded" placeholder="Room Name" />
+                        <label className="ms-2 text-sm font-medium text-gray-900 capitalize">
+                            Spa
+                        </label>
+                        <div aria-live="polite" aria-atomic="true">
+                            <span className="text-sm text-red-500 mt-2">message</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-span-4 bg-white p-4">
+                    <label htmlFor="input-file" className="flex flex-col mb-4 items-center justify-center aspect-video border-2 border-gray-300 border-dashed rounded-md cursor-pointer bg-gray-50 relative">
+                        <div className="flex flex-col items-center justify-center text-gray-500 pt-5 pb-6 z-10">
+                            <IoCloudUploadOutline className='size-8' />
+                            <div className="flex flex-col items-center justify-center">
+                                <p className="mb-1 text-sm font-bold">Select Image</p>
+                                <p className="text-xs">SVG, PNG, JPG, GIF or Other (Max: 4MB)</p>
+                            </div>
+                        </div>
+
+                        <input type="file" id='input-file' className='hidden' />
+                    </label>
+                    <div className="mb-4">
+                        <input type="text" name="capacity" className="py-2 px-4 rounded-sm border border-gray-400 w-full" placeholder="Capacity" />
+                        <div aria-live="polite" aria-atomic="true">
+                            <span className="text-sm text-red-500 mt-2">message</span>
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <input type="text" name="price" className="py-2 px-4 rounded-sm border border-gray-400 w-full" placeholder="Price..." />
+                        <div aria-live="polite" aria-atomic="true">
+                            <span className="text-sm text-red-500 mt-2">message</span>
+                        </div>
+                    </div>
+                    <button type='submit' className='bg-orange-400 text-white w-full hover:bg-orange-500 py-2.5 px-6 md:px-10 text-lg font-semibold cursor-pointer'>Save</button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default CreateForm
