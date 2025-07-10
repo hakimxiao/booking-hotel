d79e6771878e6b88477d1df249aa78135e816c48
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-07-10 16:59:41 +0700

    Melakukkan optimasi, Karena CreateForm memerlukan data fetching untuk menampilkan amenities tetapi tidak kita implement di page karena dapat membuat navigasi ke halaman create menjadi terhalang. - Solusinya adalah dengan suspen dan untuk mendapatkan suspen data fetch nya harus kita letakkan di bawah page, sehingga alurnya page(Create-Room)->createRoom(CreateForm)

--- app/admin/room/create/page.tsx
+++ app/admin/room/create/page.tsx
@@ -1,9 +1,9 @@
-import CreateForm from "@/components/admin/room/CreateForm"
+import CreateRooom from "@/components/admin/room/Create-Rooom";
 
 const CreateRoomPage = () => {
     return (
         <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
-            <CreateForm />
+            <CreateRooom />
         </div>
     )
 }
