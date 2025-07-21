1f91006fae97cddf192e91282dfed6d3c4a6c8d7
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-07-22 00:21:50 +0700

    Membuat component CheckoutDetail Yang mengambil data via db melalui function yang ada di data.ts kemudian || meletakkan component tadi didalam route reservation/[id] yang dimana dia dikirim props dari dynamic route

--- app/checkout/[id]/page.tsx
+++ app/checkout/[id]/page.tsx
@@ -1,9 +1,17 @@
+import CheckoutDetail from "@/components/CheckoutDetail";
 import React from "react";
 
-const CheckoutPage = () => {
+const CheckoutPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const reservationId = (await params).id;
+
   return (
     <div className="max-w-screen-xl px-4 mx-auto py-20 mt-12">
       <h1>Checkout Page</h1>
+      <CheckoutDetail reservationId={reservationId} />
     </div>
   );
 };
